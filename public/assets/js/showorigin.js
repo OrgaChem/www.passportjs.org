5f415ba1a68d86e011ff3a5f14d4bf8bc33cf03a
OrgaChem <orga.chem.job@gmail.com>
2014-02-20 05:54:31 +0900

    Improve parallel translation UI

--- public/assets/js/showorigin.js
+++ public/assets/js/showorigin.js
@@ -1,16 +1,14 @@
 var origins = $('.original');
 origins.each(function(idx, elem) {
   var origin = $(elem);
-  var button = $(document.createElement('button'));
-  button.addClass('btn btn-mini btn-info');
-  var buttonContent = $(document.createElement('span'));
-  buttonContent.addClass('show-origin');
-  buttonContent.html('&raquo; 原文');
 
-  button.append(buttonContent);
-  button.on('click', function() {
-    origin.show();
+  var btn = $(document.createElement('button'));
+  btn.addClass('btn btn-mini btn-link');
+  btn.html('&raquo; 原文');
+
+  btn.on('click', function() {
+    origin.toggle();
   });
   var translation = origin.prev();
-  translation.append(button);
+  translation.append(btn);
 });
